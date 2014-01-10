stolenricecakes/brewpi-avr

commit bc9409360911719da9e71fa4610b626c493e5948

fixes for detecting onewire sensor resets. Fixes also for changes to TempSensor, in particular, the failedReadCount is set to -1 (mandatory re-init) when the sensor is assigned.

brewpi_avr/DeviceManager.cpp
@@ -857,7 +857,8 @@ void UpdateDeviceState(DeviceDisplay& dd, DeviceConfig& dc, char* val)
 		else if (dt==DEVICETYPE_TEMP_SENSOR) {
 			BasicTempSensor& s = unwrapSensor(dc.deviceFunction, *ppv);
 			temperature temp = s.read();
-			fixedPointToString(val, temp, 3, 9);
+			char tempString[9];
+			tempToString(tempString, temp, 3, 9);
 		}
 		else if (dt==DEVICETYPE_SWITCH_ACTUATOR) {
 			sprintf_P(val, STR_FMT_U, (unsigned int) ((Actuator*)*ppv)->isActive()!=0);			

brewpi_avr/DallasTemperature.cpp
@@ -48,26 +48,38 @@ bool DallasTemperature::initConnection(const uint8_t* deviceAddress) {
 #if REQUIRESRESETDETECTION
 	ScratchPad scratchPad;
 	
-	if (!isConnected(deviceAddress, scratchPad))
+	if (!isConnected(deviceAddress, scratchPad)) {
 		return false;
+	}
 		
+	// assume the sensor has just been powered on. So this should only be called on initializtion, or
+	// after a device was disconnected.			
 	if (scratchPad[HIGH_ALARM_TEMP]) {		// conditional to avoid wear on eeprom. 			
 		scratchPad[HIGH_ALARM_TEMP] = 0;
 		writeScratchPad(deviceAddress, scratchPad, true);	// save to eeprom
 		
 		// check if the write was successful (HIGH_ALARAM_TEMP == 0)
 		if (!isConnected(deviceAddress, scratchPad) || !detectedReset(scratchPad))
-			return false;
-		
-		scratchPad[HIGH_ALARM_TEMP] = 1;
-		writeScratchPad(deviceAddress, scratchPad, false);	// don't save to eeprom, so that it reverts to 0 on reset
-		// from this point on, if we read a scratchpad with a 0 value in HIGH_ALARM (detectedReset() returns true)
-		// it means the device has reset or the previous write of the scratchpad above was unsuccessful.
-		// Either way, initConnection() should be called again
-		// This is checked for in isConnected()
+			return false;		
 	}
-	return true;
+
+	scratchPad[HIGH_ALARM_TEMP]=1;
+	writeScratchPad(deviceAddress, scratchPad, false);	// don't save to eeprom, so that it reverts to 0 on reset
+	// from this point on, if we read a scratchpad with a 0 value in HIGH_ALARM (detectedReset() returns true)
+	// it means the device has reset or the previous write of the scratchpad above was unsuccessful.
+	// Either way, initConnection() should be called again	
 #endif	
+	return true;
+}
+
+bool DallasTemperature::detectedReset(const uint8_t* scratchPad)
+{
+	#if REQUIRESRESETDETECTION
+	bool reset = (scratchPad[HIGH_ALARM_TEMP]==0);
+	return reset;
+	#else
+	return false;
+	#endif
 }
 
 #if REQUIRESDEVICEENUM
@@ -145,20 +157,24 @@ bool DallasTemperature::isConnected(const uint8_t* deviceAddress)
 }
 
 // attempt to determine if the device at the given address is connected to the bus
-// also allows for updating the read scratchpad
+// also allows for updating the read scratchpad.
 bool DallasTemperature::isConnected(const uint8_t* deviceAddress, uint8_t* scratchPad)
 {
     readScratchPad(deviceAddress, scratchPad);
     return (_wire->crc8(scratchPad, 8) == scratchPad[SCRATCHPAD_CRC]);
 }
 
+void DallasTemperature::sendCommand(const uint8_t* deviceAddress, uint8_t command) {
+    _wire->reset();
+    _wire->select(deviceAddress);
+    _wire->write(command);	
+}
+
 // read device's scratch pad
 void DallasTemperature::readScratchPad(const uint8_t* deviceAddress, uint8_t* scratchPad)
 {
     // send the command
-    _wire->reset();
-    _wire->select(deviceAddress);
-    _wire->write(READSCRATCH);
+	sendCommand(deviceAddress, READSCRATCH);
 
     // TODO => collect all comments &  use simple loop
     // byte 0: temperature LSB
@@ -174,11 +190,12 @@ void DallasTemperature::readScratchPad(const uint8_t* deviceAddress, uint8_t* sc
     //         DS18B20 & DS1822: store for crc
     // byte 8: SCRATCHPAD_CRC
     //
-    // for(int i=0; i<9; i++)
-    // {
-    //   scratchPad[i] = _wire->read();
-    // }
-
+#if 1			// saves 96 bytes using the loop
+     for(int i=0; i<9; i++)
+     {
+       scratchPad[i] = _wire->read();
+     }
+#else
 
     // read the response
 
@@ -216,21 +233,20 @@ void DallasTemperature::readScratchPad(const uint8_t* deviceAddress, uint8_t* sc
     // byte 8:
     // SCTRACHPAD_CRC
     scratchPad[SCRATCHPAD_CRC] = _wire->read();
-
+#endif
     _wire->reset();
 }
 
 // writes device's scratch pad
 void DallasTemperature::writeScratchPad(const uint8_t* deviceAddress, const uint8_t* scratchPad, boolean copyToEeprom)
 {
-    _wire->reset();
-    _wire->select(deviceAddress);
-    _wire->write(WRITESCRATCH);
-    _wire->write(scratchPad[HIGH_ALARM_TEMP]); // high alarm temp
-    _wire->write(scratchPad[LOW_ALARM_TEMP]); // low alarm temp
-    // DS18S20 does not use the configuration register
-    if (!isDS18S20Model(deviceAddress)) 
-		_wire->write(scratchPad[CONFIGURATION]); // configuration
+	sendCommand(deviceAddress, WRITESCRATCH);
+    
+	// DS18S20 does not use the configuration register
+	uint8_t top = isDS18S20Model(deviceAddress) ? LOW_ALARM_TEMP : CONFIGURATION;
+	for (uint8_t i=HIGH_ALARM_TEMP; i<=top; i++) {
+		_wire->write(scratchPad[i]); // high alarm temp
+	}
     _wire->reset();
     
 	// save the newly written values to eeprom
@@ -248,9 +264,7 @@ void DallasTemperature::writeScratchPad(const uint8_t* deviceAddress, const uint
 bool DallasTemperature::readPowerSupply(const uint8_t* deviceAddress)
 {
     bool ret = false;
-    _wire->reset();
-    _wire->select(deviceAddress);
-    _wire->write(READPOWERSUPPLY);
+    sendCommand(deviceAddress, READPOWERSUPPLY);
     if (_wire->read_bit() == 0) ret = true;
     _wire->reset();
     return ret;
@@ -427,14 +441,16 @@ void DallasTemperature::requestTemperatures()
 // returns FALSE if device is disconnected
 // returns TRUE  otherwise
 bool DallasTemperature::requestTemperaturesByAddress(const uint8_t* deviceAddress)
-{
-    _wire->reset();
+{    
+	_wire->reset();
     _wire->select(deviceAddress);
     _wire->write(STARTCONVO, isParasitePowerMode());
 
     // check device
     ScratchPad scratchPad;
-    if (!isConnected(deviceAddress, scratchPad)) return false;
+    if (!isConnected(deviceAddress, scratchPad) || detectedReset(scratchPad)) {
+		return false;
+	}
 
     // ASYNC mode?
 #if REQUIRESWAITFORCONVERSION
@@ -551,7 +567,7 @@ int16_t DallasTemperature::calculateTemperature(const uint8_t* deviceAddress, ui
 int16_t DallasTemperature::getTempRaw(const uint8_t* deviceAddress)
 {
     ScratchPad scratchPad;
-    if (isConnected(deviceAddress, scratchPad) && !detectedReset(scratchPad)) return calculateTemperature(deviceAddress, scratchPad);
+    if (isConnected(deviceAddress, scratchPad) && !detectedReset(scratchPad)) return calculateTemperature(deviceAddress, scratchPad);	
     return DEVICE_DISCONNECTED;		// use a value that the sensor could not ordinarily measure
 }
 

brewpi_avr/DallasTemperature.h
@@ -57,7 +57,6 @@
 #endif
 
 
-
 // both whole bus ops and indexed address access make use of device enumeration
 #ifndef REQUIRESDEVICEENUM
 #define REQUIRESDEVICEENUM REQUIRESWHOLEBUSOPS || REQUIRESINDEXEDADDRESSING
@@ -138,14 +137,7 @@ class DallasTemperature
    * Determines if the device has been powered off since the last call to init connection. 
    * Only functional when REQUIRESRESETDETECTION is enabled, otherwise returns false always.
    */
-  bool detectedReset(const uint8_t* scratchPad) {
-#if REQUIRESRESETDETECTION
-	return scratchPad[HIGH_ALARM_TEMP]==0;
-#else
-	return false;	
-#endif
-	  
-  }
+  bool detectedReset(const uint8_t* scratchPad);
 
 #if REQUIRESDEVICEENUM
   // initialise bus
@@ -339,6 +331,8 @@ class DallasTemperature
   #endif
 
   private:
+  void sendCommand(const uint8_t* deviceAddress, uint8_t command);
+	
   typedef uint8_t ScratchPad[9];
 
 #if REQUIRESPARASITEPOWERAVAILABLE  

brewpi_avr/DisplayLcd.cpp
@@ -112,8 +112,7 @@ void LcdDisplay::printTemperatureAt(uint8_t x, uint8_t y, temperature temp){
 
 
 void LcdDisplay::printTemperature(temperature temp){
-	if (temp==INVALID_TEMP)
-	{
+	if (temp==INVALID_TEMP) {
 		lcd.print_P(PSTR(" --.-"));
 		return;
 	}

brewpi_avr/OneWireTempSensor.cpp
@@ -52,16 +52,19 @@ bool OneWireTempSensor::init(){
 		}
 	}
 	
+	logDebug("init onewire sensor");
 	// This quickly tests if the sensor is connected and initializes the reset detection.
 	// During the main TempControl loop, we don't want to spend many seconds
 	// scanning each sensor since this brings things to a halt.
 	if (sensor && sensor->initConnection(sensorAddress) && requestConversion()) {
+		logDebug("init onewire sensor - wait for conversion");
 		waitForConversion();
-		temperature temperature = readAndConstrainTemp();
-		DEBUG_ONLY(logInfoIntStringTemp(INFO_TEMP_SENSOR_INITIALIZED, pinNr, addressString, temperature));
-		success = temperature!=DEVICE_DISCONNECTED && requestConversion();
+		temperature temp = readAndConstrainTemp();
+		DEBUG_ONLY(logInfoIntStringTemp(INFO_TEMP_SENSOR_INITIALIZED, pinNr, addressString, temp));
+		success = temp!=DEVICE_DISCONNECTED && requestConversion();
 	}	
 	setConnected(success);
+	logDebug("init onewire sensor complete %d", success);
 	return success;
 }
 
@@ -92,18 +95,18 @@ temperature OneWireTempSensor::read(){
 	if (!connected)
 		return TEMP_SENSOR_DISCONNECTED;
 	
-	temperature temperature = readAndConstrainTemp();
+	temperature temp = readAndConstrainTemp();
 	requestConversion();
-	return temperature;
+	return temp;
 }
 
 temperature OneWireTempSensor::readAndConstrainTemp()
 {
-	temperature temperature = sensor->getTempRaw(sensorAddress);
-	if(temperature == DEVICE_DISCONNECTED){
+	temperature temp = sensor->getTempRaw(sensorAddress);
+	if(temp == DEVICE_DISCONNECTED){
 		setConnected(false);
 		return TEMP_SENSOR_DISCONNECTED;
 	}
-	temperature = constrainTemp(temperature+calibrationOffset+(C_OFFSET>>5), ((int) MIN_TEMP)>>5, ((int) MAX_TEMP)>>5)<<5;
-	return temperature;
+	temp = constrainTemp(temp+calibrationOffset+(C_OFFSET>>5), ((int) MIN_TEMP)>>5, ((int) MAX_TEMP)>>5)<<5;
+	return temp;
 }

brewpi_avr/TempSensor.cpp
@@ -24,11 +24,13 @@
 
 void TempSensor::init()
 {				
-	if (_sensor->init() && (failedReadCount==-1 || failedReadCount>60)) {
-		temperature temperature = _sensor->read();
-		if (temperature!=TEMP_SENSOR_DISCONNECTED) {
-			fastFilter.init(temperature);
-			slowFilter.init(temperature);
+	logDebug("tempsensor::init - begin %d", failedReadCount);
+	if (_sensor && _sensor->init() && (failedReadCount==-1 || failedReadCount>60)) {		
+		temperature temp = _sensor->read();
+		if (temp!=TEMP_SENSOR_DISCONNECTED) {
+			logDebug("initializing filters with value %d", temp);
+			fastFilter.init(temp);
+			slowFilter.init(temp);
 			slopeFilter.init(0);
 			prevOutputForSlope = slowFilter.readOutputDoublePrecision();
 			failedReadCount = 0;
@@ -39,9 +41,10 @@ void TempSensor::init()
 void TempSensor::update()
 {	
 	temperature temp;
-	if (!_sensor || (temp=_sensor->read())==TEMP_SENSOR_DISCONNECTED) {
+	if (!_sensor || (temp=_sensor->read())==TEMP_SENSOR_DISCONNECTED) {		
 		failedReadCount++;		
 		failedReadCount = min(failedReadCount,127);	// limit
+		return;
 	}
 		
 	fastFilter.add(temp);

brewpi_avr/TempSensor.h
@@ -46,13 +46,14 @@ enum TempSensorType {
 
 class TempSensor {
 	public:	
-	TempSensor(TempSensorType sensorType, BasicTempSensor* sensor =NULL) : _sensor(sensor)  {
+	TempSensor(TempSensorType sensorType, BasicTempSensor* sensor =NULL)  {
 		updateCounter = 255; // first update for slope filter after (255-4s)
-		failedReadCount = -1;
+		setSensor(sensor);
 	 }	 	 
 	 
 	 void setSensor(BasicTempSensor* sensor) {
 		 _sensor = sensor;
+		 failedReadCount = -1;
 	 }
 
 	bool hasSlowFilter() { return true; }

commit 5edbb0c843c5c97f9560ba407820336debac01d6

Fix for bug introduced in commit bc9409360911719da9e71fa4610b626c493e5948 of January 4: tempToString wrote to unused temporary array, instead of to the array that is returned.

brewpi_avr/DeviceManager.cpp
@@ -862,8 +862,7 @@ void UpdateDeviceState(DeviceDisplay& dd, DeviceConfig& dc, char* val)
 		else if (dt==DEVICETYPE_TEMP_SENSOR) {
 			BasicTempSensor& s = unwrapSensor(dc.deviceFunction, *ppv);
 			temperature temp = s.read();
-			char tempString[9];
-			tempToString(tempString, temp, 3, 9);
+			tempToString(val, temp, 3, 9);
 		}
 		else if (dt==DEVICETYPE_SWITCH_ACTUATOR) {
 			sprintf_P(val, STR_FMT_U, (unsigned int) ((Actuator*)*ppv)->isActive()!=0);			